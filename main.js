9a51211ed3a47c1ee91b2c0d1ed177b977f4487a
Kevin Lee <kevinmlee.x@gmail.com>
2020-11-09 21:51:06 -0500

    exercise 01.02.05: isolated forms

--- main.js
+++ main.js
@@ -11,11 +11,29 @@ function setup() {
 }
 
 function drawGrid() {
-  // draw grid
   for (let i = 0; i < GRID_ROWS; i++) {
     for (let j = 0; j < GRID_COLS; j++) {
       stroke(255);
-      point(i * GRID_W + GRID_W / 2 + BORDER, j * GRID_H + GRID_H / 2 + BORDER);
+      point(...gridToDrawCoord(i, j));
+    }
+  }
+}
+
+function drawLineGrid() {
+  stroke(255, 0, 0);
+  for (let i = 0; i < GRID_ROWS; i++) {
+    line(...gridToDrawCoord(0, i), ...gridToDrawCoord(GRID_COLS - 1, i));
+    for (let j = 0; j < GRID_COLS; j++) {
+      const midpt = gridToDrawCoord(j, i);
+      line(
+        midpt[0],
+        Math.max(midpt[1] - GRID_H / 8, BORDER + GRID_H / 2),
+        midpt[0],
+        Math.min(
+          midpt[1] + GRID_H / 8,
+          BORDER + GRID_H / 2 + GRID_H * (GRID_ROWS - 1),
+        ),
+      );
     }
   }
 }
@@ -26,58 +44,83 @@ function gridToDrawCoord(x, y) {
 
 // assume square grid
 const INCREMENT = 1 / GRID_COLS;
-function transform(x, y) {
-  // bottom
-  if (y == GRID_ROWS - 1) {
-    // bottom right corner
-    if (x == GRID_COLS - 1) {
-      return [x, Math.max(0, y - INCREMENT)];
-    }
-    return [Math.min(x + INCREMENT, GRID_COLS - 1), y];
-  }
-  // right
-  if (x == GRID_COLS - 1) {
-    // top right corner
-    if (y == 0) {
-      return [Math.ma(0, x - INCREMENT), y];
-    }
-    return [x, Math.max(0, y - INCREMENT)];
-  }
-  if (y == 0) {
-    // top left corner
-    if (x == 0) {
-      return [x, Math.min(y + INCREMENT, GRID_ROWS - 1)];
-    }
-    // top
-    return [Math.max(0, x - INCREMENT), y];
-  }
-  // left
-  if (x == 0) {
-    // bottom left corner
-    if (y == GRID_ROWS - 1) {
-      return [Math.min(x + INCREMENT, GRID_COLS - 1), y];
-    }
-    return [x, Math.min(y + INCREMENT, GRID_ROWS - 1)];
-  }
-}
 
-let [x1, y1] = [0, 0];
-let [x2, y2] = [0, GRID_COLS - 1];
-let [x3, y3] = [GRID_ROWS - 1, GRID_COLS - 1];
+const exercises = {
+  '01.02.02': {
+    draw: (function () {
+      let [x1, y1] = [0, 0];
+      let [x2, y2] = [0, GRID_COLS - 1];
+      let [x3, y3] = [GRID_ROWS - 1, GRID_COLS - 1];
+      return function () {
+        background(0);
+        fill(255, 0, 0);
+        noStroke();
+        beginShape();
+        vertex(...gridToDrawCoord(x1, y1));
+        vertex(...gridToDrawCoord(x2, y2));
+        vertex(...gridToDrawCoord(x3, y3));
+        endShape(CLOSE);
+
+        [x1, y1] = this.transform(x1, y1);
+        [x2, y2] = this.transform(x2, y2);
+        [x3, y3] = this.transform(x3, y3);
+
+        drawGrid();
+      };
+    })(),
+    transform: function (x, y) {
+      // bottom
+      if (y == GRID_ROWS - 1) {
+        // bottom right corner
+        if (x == GRID_COLS - 1) {
+          return [x, Math.max(0, y - INCREMENT)];
+        }
+        return [Math.min(x + INCREMENT, GRID_COLS - 1), y];
+      }
+      // right
+      if (x == GRID_COLS - 1) {
+        // top right corner
+        if (y == 0) {
+          return [Math.max(0, x - INCREMENT), y];
+        }
+        return [x, Math.max(0, y - INCREMENT)];
+      }
+      if (y == 0) {
+        // top left corner
+        if (x == 0) {
+          return [x, Math.min(y + INCREMENT, GRID_ROWS - 1)];
+        }
+        // top
+        return [Math.max(0, x - INCREMENT), y];
+      }
+      // left
+      if (x == 0) {
+        // bottom left corner
+        if (y == GRID_ROWS - 1) {
+          return [Math.min(x + INCREMENT, GRID_COLS - 1), y];
+        }
+        return [x, Math.min(y + INCREMENT, GRID_ROWS - 1)];
+      }
+    },
+  },
+  '01.02.05': {
+    draw: function () {
+      background(255);
+      fill(0);
+      noStroke();
+      beginShape();
+      vertex(...gridToDrawCoord(0, 0));
+      vertex(...gridToDrawCoord(0, GRID_ROWS - 1));
+      vertex(...gridToDrawCoord(GRID_COLS / 8, GRID_ROWS - 1));
+      vertex(...gridToDrawCoord(GRID_COLS - 1, GRID_ROWS / 2));
+      vertex(...gridToDrawCoord(GRID_COLS - 1, 0));
+      endShape(CLOSE);
+      drawLineGrid();
+    },
+  },
+};
 
 function draw() {
-  background(0);
-  fill(255, 0, 0);
-  noStroke();
-  beginShape();
-  vertex(...gridToDrawCoord(x1, y1));
-  vertex(...gridToDrawCoord(x2, y2));
-  vertex(...gridToDrawCoord(x3, y3));
-  endShape(CLOSE);
-
-  [x1, y1] = transform(x1, y1);
-  [x2, y2] = transform(x2, y2);
-  [x3, y3] = transform(x3, y3);
-
-  drawGrid();
+  exercises['01.02.02'].draw();
+  exercises['01.02.05'].draw();
 }
