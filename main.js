ebc3f91a5067c71142df57ea268280e78d712777
Kevin Lee <kevinmlee.x@gmail.com>
2020-11-13 23:52:44 -0500

    04, generalize grid and forms system

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const randInt = (max) => Math.round(Math.random() * max);
+
 let GRID_ROWS = 9;
 let GRID_COLS = 9;
 const CANVAS_W = 400;
@@ -38,16 +40,18 @@ function drawLineGrid() {
   }
 }
 
-function gridToDrawCoord(
-  x,
-  y,
+function configureGrid(
   grid_w = GRID_W,
   grid_h = GRID_H,
   offset_x = BORDER,
   offset_y = BORDER,
 ) {
-  return [x * grid_w + offset_x, y * grid_h + offset_y];
+  return (x, y) => {
+    return [x * grid_w + offset_x, y * grid_h + offset_y];
+  };
 }
+
+const gridToDrawCoord = configureGrid();
 
 const ccw_transform = (x, y) => {
   // bottom
@@ -84,6 +88,70 @@ const ccw_transform = (x, y) => {
   }
 };
 
+const formTemplates = [
+  [
+    [0, 0],
+    [0, 8],
+    [1, 8],
+    [8, 4],
+    [8, 0],
+  ],
+  [
+    [0, 1],
+    [6, 0],
+    [8, 0],
+    [8, 5],
+    [7, 8],
+    [1, 8],
+    [0, 6],
+  ],
+  [
+    [0, 3],
+    [1, 0],
+    [8, 0],
+    [8, 7],
+    [6, 8],
+    [1, 8],
+    [0, 7],
+  ],
+];
+
+const formFromTemplate = (formTemplate) => ({
+  fillColor,
+  grid_rows,
+  grid_cols,
+  grid_w,
+  grid_h,
+  offset_x,
+  offset_y,
+}) => {
+  const gtdc = configureGrid(grid_w, grid_h, offset_x, offset_y);
+  fill(...(Array.isArray(fillColor) ? fillColor : [fillColor]));
+  noStroke();
+  beginShape();
+  formTemplate.forEach(([x, y]) => {
+    vertex(...gtdc((x * (grid_cols - 1)) / 8, (y * (grid_rows - 1)) / 8));
+  });
+  endShape(CLOSE);
+};
+
+const forms = formTemplates.map(formFromTemplate);
+
+const randomSeptaForm = () => {
+  const xset = [randInt(4), 4 + randInt(4)];
+  const lset = [randInt(4), 4 + randInt(4)];
+  const rset = [randInt(4), 4 + randInt(4)];
+  return formFromTemplate([
+    [0, lset[0]],
+    [randInt(8), 0],
+    [8, rset[0]],
+    [8, rset[1]],
+    [xset[1], 8],
+    [xset[0], 8],
+    [0, lset[1]],
+  ]);
+};
+
 // assume square grid
 const INCREMENT = 1 / GRID_COLS;
 
@@ -114,15 +182,7 @@ const exercises = {
   '01.02.05': {
     draw: function () {
       background(255);
-      fill(0);
-      noStroke();
-      beginShape();
-      vertex(...gridToDrawCoord(0, 0));
-      vertex(...gridToDrawCoord(0, GRID_ROWS - 1));
-      vertex(...gridToDrawCoord((GRID_COLS - 1) / 8, GRID_ROWS - 1));
-      vertex(...gridToDrawCoord(GRID_COLS - 1, (GRID_ROWS - 1) / 2));
-      vertex(...gridToDrawCoord(GRID_COLS - 1, 0));
-      endShape(CLOSE);
+      forms[0]({fillColor: 0, grid_rows: 9, grid_cols: 9});
       drawLineGrid();
     },
   },
@@ -176,11 +236,68 @@ const exercises = {
       };
     })(),
   },
+  '030': {
+    designation: '01.02.05 isolated forms',
+    draw: (() => {
+      form2 = randomSeptaForm();
+      return () => {
+        background(255);
+        forms[1]({
+          fillColor: [255, 0, 0],
+          grid_rows: 9,
+          grid_cols: 9,
+          grid_w: (GRID_W * 4) / 8,
+          grid_h: (GRID_H * 4) / 8,
+        });
+        form2({
+          fillColor: [255, 0, 0],
+          grid_rows: 9,
+          grid_cols: 9,
+          grid_w: (GRID_W / 8) * 2,
+          grid_h: (GRID_H / 8) * 2,
+          offset_x: BORDER + GRID_W * 4,
+        });
+      };
+    })(),
+  },
+  '030a': {
+    designation: '01.02.05 isolated forms',
+    draw: (() => {
+      generatedForms = Array(4).fill(0).map(randomSeptaForm);
+      return () => {
+        GRID_COLS = 11;
+        GRID_ROWS = 11;
+        GRID_W = CANVAS_W / (GRID_ROWS - 1);
+        GRID_H = CANVAS_H / (GRID_COLS - 1);
+        background(255);
+        forms[1]({
+          fillColor: [255, 0, 0],
+          grid_rows: 9,
+          grid_cols: 9,
+          grid_w: (GRID_W * 2) / 8,
+          grid_h: (GRID_H * 2) / 8,
+          offset_y: BORDER + GRID_H * 4,
+        });
+        for (let i = 1; i < 5; i++) {
+          generatedForms[i - 1]({
+            fillColor: 0,
+            grid_rows: 9,
+            grid_cols: 9,
+            grid_w: (GRID_W * 2) / 8,
+            grid_h: (GRID_H * 2) / 8,
+            offset_x: BORDER + GRID_W * 2 * i,
+            offset_y: BORDER + GRID_H * 4,
+          });
+        }
+      };
+    })(),
+  },
 };
 
 function draw() {
   //exercises['01.02.02'].draw();
   //exercises['01.02.05'].draw();
   //exercises['01.02.05a'].draw();
-  exercises['029'].draw();
+  //exercises['029'].draw();
+  exercises['030a'].draw();
 }
